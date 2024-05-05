13cf8436906b8fe9d0869528b5c7213b4b71d0e6
Your Name <you@example.com>
2024-05-04 21:42:34 -0600

    My firts commit

---
+++ demo-da23025/src/main/java/com/iis/da23025/demo_da23025/App.java
@@ -0,0 +1,13 @@
+package com.iis.da23025.demo_da23025;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demo-da23025/src/test/java/com/iis/da23025/demo_da23025/AppTest.java
@@ -0,0 +1,20 @@
+package com.iis.da23025.demo_da23025;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
